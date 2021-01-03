a66bafca0928296141ffa0668c2c809d3e4ccee3
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2021-01-03 13:26:33 +0100

    created spring security initializer

---
+++ spring-security-demo-01-base-app/src/main/java/com/javi/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.javi.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
